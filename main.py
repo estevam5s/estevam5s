920784e8d6f677b62f6deeacc29f9e9e5268149a
Estevamsl <estevamsouzalaureth@gmail.com>
2022-10-16 16:27:09 -0300

    :construction: in progress

--- main.py
+++ main.py
@@ -17,11 +17,11 @@ def makeCommits (days):
         # days * makeCommits(days - 1)
 
 cont = 0
-maxNumero = 3500
+maxNumero = 100
 while True:
     # makeCommits(1)
-    makeCommits(3500 - maxNumero)
+    makeCommits(100 - maxNumero)
     cont += 1
     maxNumero -= 1
-    if cont == 3500:
+    if cont == 100:
         break
